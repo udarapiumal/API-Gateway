f92fe5b6c972fd6cdd86d720aa68f79c45b7ef11
udarapiumal <udarapiumal0@gmail.com>
2026-06-17 20:22:44 +0530

    reverseProxyServer

---
+++ ReverseProxy/ReverseProxyMiddleware.cs
@@ -0,0 +1,111 @@
+﻿namespace ReverseProxy
+{
+    public class ReverseProxyMiddleware
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly RequestDelegate _nextMiddleware;
+
+        public ReverseProxyMiddleware(RequestDelegate nextMiddleware)
+        {
+            _nextMiddleware = nextMiddleware;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var targetUri = BuildTargetUri(context.Request);
+
+            if(targetUri!= null)
+            {
+                var targetRequestMessage = CreateRequestMessage(context, targetUri);
+
+                using (var responseMessage = await _httpClient.SendAsync(targetRequestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
+                {
+                    context.Response.StatusCode = (int)responseMessage.StatusCode;
+                    CopyFromTargetResponseHeaders(responseMessage, context);
+
+                    await responseMessage.Content.CopyToAsync(context.Request.Body);
+
+                }
+                return;
+
+            }
+
+
+
+            await _nextMiddleware(context);
+
+        }
+
+
+        private Uri BuildTargetUri(HttpRequest request)
+        {
+            Uri targeturi = null;
+
+            if(request.Path.StartsWithSegments("/googleforms",out var remainingPath ))
+            {
+                targeturi = new Uri("https://docs.google.com/forms" + remainingPath);
+            }
+            return targeturi;
+        }
+
+        private HttpRequestMessage CreateRequestMessage(HttpContext context,Uri targetUri)
+        {
+            var requestMessage = new HttpRequestMessage();
+
+
+            CopyFromOriginalContentAndHeaders(context,requestMessage);
+
+            requestMessage.RequestUri = targetUri;
+            requestMessage.Headers.Host = targetUri.Host;
+            requestMessage.Method = GetMethod(context.Request);
+
+            return requestMessage;
+        }
+
+        private void CopyFromOriginalContentAndHeaders(HttpContext context,HttpRequestMessage requestMessage)
+        {
+            var requestMethod = context.Request.Method;
+
+            if (!HttpMethods.IsGet(requestMethod) &&
+                !HttpMethods.IsHead(requestMethod) &&
+                !HttpMethods.IsDelete(requestMethod) &&
+                !HttpMethods.IsTrace(requestMethod)
+                
+                )
+            {
+                var streamContent = new StreamContent(context.Request.Body);
+            }
+
+            foreach(var header in context.Request.Headers)
+            {
+                requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+            }
+        }
+
+        private static HttpMethod GetMethod(HttpRequest request)
+        {
+            var someMethod = new HttpMethod(request.Method);
+
+            if (HttpMethods.IsGet(request.Method)) return HttpMethod.Get;
+            if (HttpMethods.IsHead(request.Method)) return HttpMethod.Head;
+            if (HttpMethods.IsDelete(request.Method)) return HttpMethod.Delete;
+            if (HttpMethods.IsPut(request.Method)) return HttpMethod.Put;
+            if (HttpMethods.IsPost(request.Method)) return HttpMethod.Post;
+            if (HttpMethods.IsOptions(request.Method)) return HttpMethod.Options;
+            if (HttpMethods.IsTrace(request.Method)) return HttpMethod.Trace;
+
+            return someMethod;
+
+        }
+        private void CopyFromTargetResponseHeaders(HttpResponseMessage responseMessage,HttpContext context)
+        {
+            foreach(var header in responseMessage.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+            context.Response.Headers.Remove("transfer-encoding");
+        }
+    }
+
+
+}
